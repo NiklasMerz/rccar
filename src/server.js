6bfa55728f7d737ee210686945ab9e86c4821554
Niklas <nm4500@rhoen.de>
2016-06-26 22:55:43 +0200

    Blynk

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ var BlynkLib = require('blynk-library');
  
 var blynk = new BlynkLib.Blynk(process.env.APIKEY_BLYNK);
 var v1 = new blynk.VirtualPin(1);
+var v2 = new blynk.VirtualPin(2);
 var v9 = new blynk.VirtualPin(9);
  
 v1.on('write', function(param) {
   console.log('V1:', param);
+});
+
+v2.on('write', function(param) {
+  console.log('V2:', param);
 });
  
 v9.on('read', function() {
