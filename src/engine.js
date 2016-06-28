331e34197c2ee42bd251881b48dc57560fe16c87
Niklas <nm4500@rhoen.de>
2016-06-28 20:18:40 +0200

    Egnine.js start

---
+++ src/engine.js
@@ -0,0 +1,15 @@
+exports.forward = function(){
+  console.log('Move Forwards');
+}
+
+exports.backward = function(){
+  console.log('Move Backwards');
+}
+
+exports.left = function(){
+  console.log('Move left');
+}
+
+exports.right = function(){
+  console.log('Move right');
+}
