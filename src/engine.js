900644d6403bdb1a10829bdb357e31860a2573a3
Niklas <nm4500@kesselflicker.net>
2016-06-30 17:58:53 +0200

    First Try

--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,11 @@
+var GPIO = require('onoff').Gpio;
+var motor1_a = new GPIO(17, 'out');
+var motor1_b = new GPIO(18, 'out');
+var motor2_a = new GPIO(22, 'out');
+var motor2_b = new GPIO(23, 'out');
+
 exports.forward = function(){
-  console.log('Move Forwards');
+  motor1_a.writeSync(1);
 }
 
 exports.backward = function(){
