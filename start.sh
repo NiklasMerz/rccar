5e907bbfae1352bdbb0d02a222ff1450b5cfa7fc
Niklas <nm4500@kesselflicker.net>
2016-07-02 09:34:08 +0200

    Camera switch

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
-echo "Starting apache"
-service apache2 start
+# Check if camer is activated
+if [ "$CAMERA_ENABLED" = "true" ]
+  echo "Starting apache"
+  service apache2 start
 
-echo "Startng camera"
-cd RPi_Cam_Web_Interface && ./start.sh
+  echo "Starting camera"
+  cd RPi_Cam_Web_Interface && ./start.sh
+fi
 
 echo "Starting NodeJS"
 cd .. && npm start
-
