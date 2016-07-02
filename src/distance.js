e173e8ed7cfdb40a261a9c661f3bfdd9a31855a0
Niklas <nm4500@rhoen.de>
2016-07-02 23:46:33 +0200

    New sensor

--- src/distance.js
+++ src/distance.js
@@ -6,10 +6,10 @@ var usonic_echo = 25;
 exports.getDistance = function(){
   usonic.init(function (error) {
     if (error) {
-        return 'ERROR';
+      return 'ERROR';
     } else {
-        var sensor = usonic.createSensor(usonic_echo, usonic_trig, 450);
-        return sensor();
+      var sensor = usonic.createSensor(usonic_echo, usonic_trig, 450);
+      return sensor();
     }
-});
+  });
 }
