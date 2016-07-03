750f6c7d5070c165e2371a4fef8d29e895e556bd
Niklas <nm4500@rhoen.de>
2016-07-03 09:31:46 +0200

    New Usonic module

--- src/distance.js
+++ src/distance.js
@@ -1,13 +1,18 @@
-var Sonar = require('raspi-sonar').Sonar;
-var sonarPin1 = new Sonar(25);
+var usonic = require('mmm-usonic');
+var sonarPin1 = new Sonar(24);
 
 var usonic_trig = 24;
 var usonic_echo = 25;
 
 exports.getDistance = function(){
-  sonarPin1.read(function(duration) {
-    var distance = 343.0 * duration / 1000000 * .5;
-    console.log('duration: ' + duration + ' distance: ' + distance + 'm');
-  });
-  return distance;
+  usonic.init(function (error) {
+    if (error) {
+        return "error";
+    } else {
+        var sensor = usonic.createSensor(usonic_echo, usonic_trig, 450);
+        var distance = sensor();
+        console.log(distance);
+        return distance;
+    }
+});
 }
