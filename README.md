c95d8f7973983bd2615dba707525b4ebc82c041b
Niklas <nm4500@kesselflicker.net>
2016-07-29 01:07:42 +0200

    Piblaster

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 Extension of raspbian resin.io image with nodejs
 
 Uses RPICamera: https://github.com/NiklasMerz/RPi_Cam_Web_Interface
+
+### Components
+* raspbian
+* nodejs
+* RPi Cam Webinterface
+* Piblaster - PWM
--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+#Build dependancys
 RUN apt-get -q update
 RUN apt-get install psmisc libraspberrypi-bin build-essential -y
 
 git submodule init && git submodule update
+
+#RPI Webinterface
 cd /usr/src/app/RPi_Cam_Web_Interface
 chmod u+x *.sh
 ./install.sh q
+
+# Piblaster
+sudo apt-get install pi-blaster
