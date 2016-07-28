3f6befba449c4d0db509333e4b4f34c25a0a122d
Niklas <nm4500@kesselflicker.net>
2016-07-29 00:57:29 +0200

    Try pwm

--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,7 @@
 var GPIO = require('onoff').Gpio;
+var Piblaster = require('pi-blaster.js');
 var Distance = require('./distance');
-var motor1_a = new GPIO(17, 'out');
+//var motor1_a = new GPIO(17, 'out');
 var motor1_b = new GPIO(18, 'out');
 var motor2_a = new GPIO(22, 'out');
 var motor2_b = new GPIO(23, 'out');
@@ -12,7 +13,8 @@ exports.forward = function(autostop){
     autostopListener();
   }
 
-  motor1_a.writeSync(1);
+  //motor1_a.writeSync(1);
+  Piblaster.setPwm(17, 0.1 );
 }
 
 exports.backward = function(){
@@ -28,14 +30,15 @@ exports.right = function(){
 }
 
 exports.freeRessources = function(){
-  motor1_a.unexport();
+  //motor1_a.unexport();
   motor1_b.unexport();
   motor2_a.unexport();
   motor2_b.unexport();
 }
 
 exports.stop = function(){
-  motor1_a.writeSync(0);
+  //motor1_a.writeSync(0);
+  Piblaster.setPwm(17, 0 );
   motor1_b.writeSync(0);
   motor2_a.writeSync(0);
   motor2_b.writeSync(0);
