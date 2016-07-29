921c712bc19465f88bf4ef161f756d4b3ae2719f
Niklas <nm4500@kesselflicker.net>
2016-07-29 17:47:33 +0200

    Removed pi-blaster

--- src/engine.js
+++ src/engine.js
@@ -1,7 +1,6 @@
 var GPIO = require('onoff').Gpio;
-var Piblaster = require('pi-blaster.js');
 var Distance = require('./distance');
-//var motor1_a = new GPIO(17, 'out');
+var motor1_a = new GPIO(17, 'out');
 var motor1_b = new GPIO(18, 'out');
 var motor2_a = new GPIO(22, 'out');
 var motor2_b = new GPIO(23, 'out');
@@ -13,10 +12,7 @@ exports.forward = function(autostop){
     autostopListener();
   }
 
-  //motor1_a.writeSync(1);
-  //TODO WHY BACKWWARDS???????
-  //console.log('forward');
-  Piblaster.setPwm(17, 1 );
+  motor1_a.writeSync(1);
 }
 
 exports.backward = function(){
@@ -32,15 +28,14 @@ exports.right = function(){
 }
 
 exports.freeRessources = function(){
-  //motor1_a.unexport();
+  motor1_a.unexport();
   motor1_b.unexport();
   motor2_a.unexport();
   motor2_b.unexport();
 }
 
 exports.stop = function(){
-  //motor1_a.writeSync(0);
-  Piblaster.setPwm(17, 0 );
+  motor1_a.writeSync(0);
   motor1_b.writeSync(0);
   motor2_a.writeSync(0);
   motor2_b.writeSync(0);
