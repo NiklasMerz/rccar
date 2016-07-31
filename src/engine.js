b750b756473aff1ccfa6e9476753dcb358c8aea3
Niklas <nm4500@kesselflicker.net>
2016-07-31 10:33:33 +0200

    RemovedPWM

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,11 @@
 var GPIO = require('onoff').Gpio;
-var rpio = require('rpio');
 var Distance = require('./distance');
 var motor1_a = new GPIO(17, 'out');
-//var motor1_b = new GPIO(18, 'out');
+var motor1_b = new GPIO(18, 'out');
 var motor2_a = new GPIO(22, 'out');
 var motor2_b = new GPIO(23, 'out');
 
-//TODO PWM
+//TODO PWM !!
 exports.forward = function(autostop){
 
   if(autostop){
@@ -17,20 +16,7 @@ exports.forward = function(autostop){
 }
 
 exports.backward = function(){
-  //motor1_b.writeSync(1);
-
-  var pin = 12;           /* P12/GPIO18 */
-  var range = 4096;       /* LEDs can quickly hit max brightness, so only use */
-  var max = 2048;          /*   the bottom 8th of a larger scale */
-  var clockdiv = 8;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
-
-  /*
-   * Enable PWM on the chosen pin and set the clock and range.
-   */
-  rpio.open(pin, rpio.PWM);
-  rpio.pwmSetClockDivider(clockdiv);
-  rpio.pwmSetRange(pin, range);
-  rpio.pwmSetData(12, 512);
+  motor1_b.writeSync(1);
 }
 
 exports.left = function(){
@@ -43,14 +29,14 @@ exports.right = function(){
 
 exports.freeRessources = function(){
   motor1_a.unexport();
-  //motor1_b.unexport();
+  motor1_b.unexport();
   motor2_a.unexport();
   motor2_b.unexport();
 }
 
 exports.stop = function(){
   motor1_a.writeSync(0);
-  //motor1_b.writeSync(0);
+  motor1_b.writeSync(0);
   motor2_a.writeSync(0);
   motor2_b.writeSync(0);
 }
